7a22911821f80b856b80edd613449f02dcd96d72
Carlos Panato <ctadeu@gmail.com>
2022-02-18 09:51:20 +0100

    test

--- terraform/simple.tf
+++ terraform/simple.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+  instance_type   = "t2.micro"
 
   tags = {
     # Name = "ExampleAppServerInstance"
