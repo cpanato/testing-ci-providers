cb975b173f05f1c404326c8a7231040d5fd5a8d7
cpanato <ctadeu@gmail.com>
2022-12-12 12:09:40 +0100

    rem,ove crane for now

--- scripts/e2e-kms.sh
+++ scripts/e2e-kms.sh
@@ -16,7 +16,7 @@ TEST_INSTANCE_REPO="${BASE_TEST_REPO}/$(date +'%Y/%m/%d')/$RANDOM"
 
 ## KMS using env variables!
 TEST_KMS=${TEST_KMS:-hashivault://test}
-(crane delete $(./cosign triangulate $img)) || true
+# (crane delete $(./cosign triangulate $img)) || true
 COSIGN_KMS=$TEST_KMS ./cosign/cosign generate-key-pair
 signing_key=$TEST_KMS
 
@@ -30,7 +30,7 @@ COSIGN_KEY=${verification_key} ./cosign/cosign verify -a foo=bar $img
 
 # store signatures in a different repo
 export COSIGN_REPOSITORY=${TEST_INSTANCE_REPO}/subbedrepo
-(crane delete $(./cosign triangulate $img)) || true
+# (crane delete $(./cosign triangulate $img)) || true
 COSIGN_KEY=${signing_key} ./cosign/cosign sign $img
 COSIGN_KEY=${verification_key} ./cosign/cosign verify $img
 unset COSIGN_REPOSITORY
@@ -38,7 +38,7 @@ unset COSIGN_REPOSITORY
 # test stdin interaction for private key password
 stdin_password=${COSIGN_PASSWORD}
 unset COSIGN_PASSWORD
-(crane delete $(./cosign triangulate $img)) || true
+# (crane delete $(./cosign triangulate $img)) || true
 echo $stdin_password | ./cosign/cosign sign --key ${signing_key} --output-signature interactive.sig  $img
 COSIGN_KEY=${verification_key} COSIGN_SIGNATURE=interactive.sig ./cosign/cosign verify $img
 export COSIGN_PASSWORD=${stdin_password}
