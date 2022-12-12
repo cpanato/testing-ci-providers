8f00f9277892b99349758d0a4d390ea1becaefdb
cpanato <ctadeu@gmail.com>
2022-12-12 12:19:30 +0100

    fvff

--- scripts/e2e-kms.sh
+++ scripts/e2e-kms.sh
@@ -1,7 +1,8 @@
 set -ex
 
-go build -o cosign ./cosign/cmd/cosign
-go build -o sget ./cosign/cmd/sget
+pushd cosign
+go build -o cosign ./cmd/cosign
+go build -o sget ./cmd/sget
 tmp=$(mktemp -d -t cosign-e2e-secrets.XXXX)
 cp cosign $tmp/
 cp sget $tmp/
@@ -17,30 +18,30 @@ TEST_INSTANCE_REPO="${BASE_TEST_REPO}/$(date +'%Y/%m/%d')/$RANDOM"
 ## KMS using env variables!
 TEST_KMS=${TEST_KMS:-hashivault://test}
 # (crane delete $(./cosign triangulate $img)) || true
-COSIGN_KMS=$TEST_KMS ./cosign/cosign generate-key-pair
+COSIGN_KMS=$TEST_KMS ./cosign generate-key-pair
 signing_key=$TEST_KMS
 
-if (./cosign/cosign verify --key ${verification_key} $img); then false; fi
-COSIGN_KEY=${signing_key} ./cosign/cosign sign $img
-COSIGN_KEY=${verification_key} ./cosign/cosign verify $img
+if (./cosign verify --key ${verification_key} $img); then false; fi
+COSIGN_KEY=${signing_key} ./cosign sign $img
+COSIGN_KEY=${verification_key} ./cosign verify $img
 
-if (./cosign/cosign verify -a foo=bar --key ${verification_key} $img); then false; fi
-COSIGN_KEY=${signing_key} ./cosign/cosign sign -a foo=bar $img
-COSIGN_KEY=${verification_key} ./cosign/cosign verify -a foo=bar $img
+if (./cosign verify -a foo=bar --key ${verification_key} $img); then false; fi
+COSIGN_KEY=${signing_key} ./cosign sign -a foo=bar $img
+COSIGN_KEY=${verification_key} ./cosign verify -a foo=bar $img
 
 # store signatures in a different repo
 export COSIGN_REPOSITORY=${TEST_INSTANCE_REPO}/subbedrepo
 # (crane delete $(./cosign triangulate $img)) || true
-COSIGN_KEY=${signing_key} ./cosign/cosign sign $img
-COSIGN_KEY=${verification_key} ./cosign/cosign verify $img
+COSIGN_KEY=${signing_key} ./cosign sign $img
+COSIGN_KEY=${verification_key} ./cosign verify $img
 unset COSIGN_REPOSITORY
 
 # test stdin interaction for private key password
 stdin_password=${COSIGN_PASSWORD}
 unset COSIGN_PASSWORD
 # (crane delete $(./cosign triangulate $img)) || true
-echo $stdin_password | ./cosign/cosign sign --key ${signing_key} --output-signature interactive.sig  $img
-COSIGN_KEY=${verification_key} COSIGN_SIGNATURE=interactive.sig ./cosign/cosign verify $img
+echo $stdin_password | ./cosign sign --key ${signing_key} --output-signature interactive.sig  $img
+COSIGN_KEY=${verification_key} COSIGN_SIGNATURE=interactive.sig ./cosign verify $img
 export COSIGN_PASSWORD=${stdin_password}
 
 # What else needs auth?
